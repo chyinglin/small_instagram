db52723a9943ad2bc50b2accc75e51f18f2cd1bc
Febe <phoebe.lin@oriente.com>
2020-03-02 23:00:57 +0800

    django with app

--- be/urls.py
+++ be/urls.py
@@ -1,3 +1,4 @@
+
 """be URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
@@ -13,9 +14,12 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.contrib import admin
 from django.urls import path
+from small_instagram.views import Helloworld
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('', Helloworld.as_view(), name='Helloworld')
 ]
+
