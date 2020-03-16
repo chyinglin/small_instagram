4a2540cbf19f0f964fd44ce41f194e4e9cbb1a1f
Febe <phoebe.lin@oriente.com>
2020-03-17 03:10:35 +0800

    set urls and view to display image

--- small_instagram/views.py
+++ small_instagram/views.py
@@ -1,5 +1,14 @@
 from django.shortcuts import render
 from django.views.generic.base import TemplateView
+from django.views.generic.list import ListView
+from small_instagram.models import Post
+
+
 # Create your views here.
 class Helloworld(TemplateView):
     template_name = 'test.html'
+
+
+class PostView(ListView):
+    model = Post
+    template_name = 'index.html'
